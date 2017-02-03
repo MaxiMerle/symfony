92fb89ef6c1e99c101e80c4279b3e57855703559
Maxime Merle <maximemerle@macbook-pro-de-maxime.home>
2017-02-03 01:14:15 +0100

    MAJ 2.5 Ajouts Admin/Livres et connexion BDD + Modifications routes

--- src/Devschool/AdminBundle/Controller/DefaultController.php
+++ src/Devschool/AdminBundle/Controller/DefaultController.php
@@ -5,9 +5,11 @@ namespace Devschool\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Devschool\AdminBundle\Form\GenreType;
 use Devschool\CinemaBundle\Repository\FilmRepository;
 use Devschool\CinemaBundle\Entity\Film;
-
+use Devschool\BiblioBundle\Entity\Genre;
+use Devschool\BiblioBundle\Entity\Livre;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -32,9 +34,10 @@ class DefaultController extends Controller
 
     {
     	$films = $this->getDoctrine()->getRepository('DevschoolCinemaBundle:Film')->findAll();
+        $livres = $this->getDoctrine()->getRepository('DevschoolBiblioBundle:Livre')->findAll();
     	$titre_de_la_page = 'Liste de Film';
         return $this->render('DevschoolAdminBundle:todo:index.html.twig',
-        	['films' => $films, 'titre' => $titre_de_la_page]
+        	['films' => $films,'livres' => $livres, 'titre' => $titre_de_la_page]
         	);
     }
             /**
@@ -171,7 +174,150 @@ class DefaultController extends Controller
 
     }
 
+      /**
+     * @Route("/admin/create/genre" )
+     */
+    public function createGenreAction(Request $request)
+    {
+        $genres = new Genre;
+        $form = $this ->createFormBuilder($genres)
+        ->add('nom', TextType::class, array('attr' => array('class' => 'fomr-control', 'style' => 'margin-bottom:15px')))
+        ->add('Ajouter un Genre', SubmitType::class, array('attr' => array('label' => 'Create Film', 'class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+        ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //Get Data
+
+            $nom = $form['nom']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genres);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Genre Ajouté !'
+                );
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('DevschoolAdminBundle:todo:createGenre.html.twig', array(
+            'form' => $form->createView()
+            ));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /**
+     * @Route("/admin/create/livre" )
+     */
+    public function createLivreAction(Request $request)
+    {
+        $livres = new Livre;
+        $form = $this ->createFormBuilder($livres)
+        ->add('titre', TextType::class, array('attr' => array('class' => 'fomr-control', 'style' => 'margin-bottom:15px')))
+        ->add('auteur', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+         ->add('nombrePages', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('Ajouter le Livre', SubmitType::class, array('attr' => array('label' => 'Create Film', 'class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+        ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //Get Data
+
+
+            $titre = $form['titre']->getData();
+            $auteur = $form['auteur']->getData();
+            $description = $form['description']->getData();
+            $nombrePages = $form['nombrePages']->getData();
+            
+        
+
+            $livres->setTitre($titre);
+            $livres->setAuteur($auteur);
+            $livres->setDescription($description);
+            $livres->setDescription($nombrePages);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($livres);
+            $em->flush();
+            $this->addFlash(
+                'notice',
+                'Livre Ajouté !'
+                );
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('DevschoolAdminBundle:todo:createLivre.html.twig', array(
+            'form' => $form->createView()
+            ));
+    }
+
+
+
+
+               /**
+     * @Route("/admin/details/{id}")
+     */
+    public function detailsLivreAction($id , Request $request)
+    {
+
+       $livres = $this->getDoctrine()
+       ->getRepository('DevschoolCinemaBundle:Livre')
+       ->find($id);
+ 
+        return $this->render('DevschoolAdminBundle:todo:detailsLivre.html.twig',
+            ['livre' => $livre]
+            );
+    }
+          /**
+      * @Route("/admin/delete/{id}", name="page_delete")
+     */
+    public function deleteLivreAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+            $livre = $em->getRepository('DevschoolBiblioBundle:Livre')->find($id);
+
+            $em->remove($livre);
+            $em->flush();
+                        $this->addFlash(
+                'notice',
+                'Livre Supprimé !'
+                );
+            return $this->redirectToRoute('admin');
+
+    }
 
 }
+
+
+
+
+
+
+
 
       
